dfa64f7f97a38e94dadc8642dec2ee2bf2f8f257
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-25 11:14:26 +0100

    Update PersonItemProcessor.java

--- spring-batch-processing/src/main/java/com/shotaroi/springbatchprocessing/PersonItemProcessor.java
+++ spring-batch-processing/src/main/java/com/shotaroi/springbatchprocessing/PersonItemProcessor.java
@@ -1,4 +1,22 @@
 package com.shotaroi.springbatchprocessing;
 
-public class PersonItemProcessor {
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.batch.item.ItemProcessor;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+    private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+    @Override
+    public Person process(final Person person) {
+        final String firstName = person.firstName().toUpperCase();
+        final String lastName = person.lastName().toUpperCase();
+
+        final Person transformedPerson = new Person(firstName, lastName);
+
+        log.info("Converting ({})", person, transformedPerson);
+
+        return transformedPerson;
+    }
+
 }
